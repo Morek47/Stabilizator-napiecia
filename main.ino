a74e00bfb1df76042d00b78a244081d45295c0ba
Morek47 <morek47@wp.pl>
2024-09-05 15:47:17 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -405,15 +405,3 @@ void displayData() {
     display.display();
 }
 
-void executeAction(int action) {
-    switch(action) {
-        case 0:
-            // Zwiększ prąd cewki wzbudzenia 1 o stałą wartość
-            analogWrite(excitationBJT1Pin, constrain(analogRead(excitationBJT1Pin) + 10, 0, MAX_EXCITATION_CURRENT));
-            break;
-        case 1:
-            // Zmniejsz prąd cewki wzbudzenia 1 o stałą wartość
-            analogWrite(excitationBJT1Pin, constrain(analogRead(excitationBJT1Pin) - 10, 0, MAX_EXCITATION_CURRENT));
-            break;
-        case 2:
-            // Zwiększ prąd cewki wzbudzenia 2 o
